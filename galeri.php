b7076411fccf6b4d138fbaed6e42089e8ae7eaa3
fatimenurcelik <36274941+fatimenurcelik@users.noreply.github.com>
2021-01-05 23:39:05 +0300

    Create galeri.php

---
+++ galeri.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>images</title>
+
+</head>
+
+<body>     
+        <img src="images/31-daire-oturma-odasi.jpg" style="width: 500px;">
+        <img src="images/31-daire-mutfak.jpg" style="width: 500px;">
+        <img src="images/31-daire-ebeveyn.jpg" style="width: 500px;">
+        <img src="images/31-daire-banyo-1.jpg"style="width: 500px;">
+        <img src="images/71433-atakent-dumankaya-konsept-halkali-2-1-104m2-peyzaj-satilik-daire.jpg" style="width: 500px; height: 380px;">
+        <img src="images/buyuk-30_206949473.jpg" style="width: 500px;">
+        <img src="miages/indir.jpg" style="width: 500px;">
+</body>
+</html>
