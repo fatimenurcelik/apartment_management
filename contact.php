7bf62117e5d77fdfb6c6c55ed9b9ea1ca60811b7
fatimenurcelik <36274941+fatimenurcelik@users.noreply.github.com>
2021-01-05 23:38:45 +0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact</title>
+</head>
+
+<body>
+    <div style="top: 50%; text-align: center; font-size: 150%;">
+        <img src="images/telephone.png" alt="telephone" style="width: 200px; height: 200px;">
+        <br>
+        <h2 > 05xx xxx xx xx</h2>
+        <h2> 05xx xxx xx xx</h2>
+    </div>
+</body>
+</html>
