7f23ece5c57caeb0557427c1e571baa8a8d6f2e6
fatimenurcelik <36274941+fatimenurcelik@users.noreply.github.com>
2021-01-05 23:37:14 +0300

    Create afteradmin.php

---
+++ afteradmin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2><a href="member.php">yeni üye ekle</a></h2>
+    <h2><a href="logout.php">cıkıs yap</a></h2>
+    <h2><a href="üyebilgi.php">üye bilgilerini göster</a></h2>
+</body>
+</html>
